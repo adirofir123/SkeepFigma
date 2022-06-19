d5966b55f75fd483eef1ccddca3a42f624c90166
Adir Ofir <adirofir123@gmail.com>
2022-06-19 16:22:07 +0300

    project

---
+++ restapi/WebApplication1/Class.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebApplication1
+{
+    public class DataClass
+    {
+        public string [] GetBlocksDate()
+        {
+            var title1 = "7500";
+            var title2 = "1247";
+            var title3 = "120";
+
+            string [] blocksdata = new string[] { title1, title2, title3 };
+            return blocksdata;
+        }
+    }
+}
---
+++ restapi/WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("GetData")]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet("[action]")]
+        public IEnumerable<string> Getblocksdata()
+        {
+            return new string[] { "7,500", "$1,247", "$120" };
+        }
+
+
+
+        // GET api/<UserController>/5
+        [HttpGet]
+        [Route("[action]")]
+        public IEnumerable<string> Getbarchartdata()
+        {
+            return new string[] { "7526", "5840", "4282","3840","3356","3024","2721","2238","1845","1064" };
+
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet]
+        [Route("[action]")]
+        public IEnumerable<string> Getdonutchartdata()
+        {
+            return new string[] { "40", "20", "7" };
+
+        }
+
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
